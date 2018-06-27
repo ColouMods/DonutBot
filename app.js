596f3fae1d19b6a69ce5996c30c74e4f01863157
DocBison <37480191+DocBison@users.noreply.github.com>
2018-06-27 16:07:19 -0400

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,8 @@
   client.on('ready', () => {
     console.log('I am ready!');
   });
- greet = ["Hello.", "Hi."];
+ greet = ["Hello", "Hi", "Hey"];
+ greet2 = ["how's it going?", "what's up?", "I don't really like you, to be honest."];
  mention = ["You called?", "Yeah?", "What do you need?", "What's up?", "Hi."];
  sorry = ["OK.", "Good.", "You'd better be sorry.", "I forgive you."];
 
@@ -13,7 +14,9 @@
  if (message.content.match(/<@435874889467953152>/i)) {
   message.channel.send("<@" + message.author.id + ">: You are not able to `!hype` for another [time] minutes.");
  }
-      
+ if (message.content.match(/hey donutbot/i)) {
+  message.channel.send(greet[(Math.floor(Math.random() * greet.length)+1)] + " " + message.author.username + ", " + greet[(Math.floor(Math.random() * greet2.length)+1)]);
+ } 
       return;
   });
 
